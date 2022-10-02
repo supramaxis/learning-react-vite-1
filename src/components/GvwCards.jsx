8b55f0fef26911bfa48ab444789fcd669d398a5a
SupraMaxis <kkssccd@gmail.com>
2022-10-02 00:52:23 -0600

    deleted commented code

--- src/components/GvwCards.jsx
+++ src/components/GvwCards.jsx
@@ -20,42 +20,6 @@ export default function GvwCards({ gvw }) {
           </a>
         </Card.Body>
       </Card>
-
-      {/* <Accordion>
-        <Row>
-          <Col md={4}>
-            <Card className="mb-3">
-              <Card.Img src={gvw.image} />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>{gvw.description}</Card.Text>
-                <Card.Text>{gvw.instructions}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-
-
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>{gvw.title}</Accordion.Header>
-          <Accordion.Body as="p" className="accordionBody">
-            {gvw.description}
-          </Accordion.Body>
-          <Accordion.Body as="p" className="accordionBody">
-            {gvw.instructions}
-          </Accordion.Body>
-          <Accordion.Body as="p" className="accordionBody">
-            <div className="flex justify-center items-center">
-              <a
-                className=" text-lg no-underline"
-                href={gvw.open_giveaway_url}
-                target="_blank">
-                View
-              </a>
-            </div>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion> */}
     </>
   );
 }
--- src/components/MCcards.jsx
+++ src/components/MCcards.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "flowbite";
 import "flowbite-react";
-import { Accordion, Card, CardGroup, Col, Row } from "react-bootstrap";
+import { Card, CardGroup, Col, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import AccordionBody from "react-bootstrap/esm/AccordionBody";
 import McSearch from "./McSearch";
-export default function MCcards({ searchResults, mc }) {
+export default function MCcards({ mc }) {
   return (
     <>
       <Card className="mb-3 mt-20 h-100">
@@ -42,64 +41,6 @@ export default function MCcards({ searchResults, mc }) {
           {mc.quality ? <Card.Text>Quality: {mc.quality}</Card.Text> : null}
         </Card.Body>
       </Card>
-
-      {/* <Accordion key={minecraft.name}>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>{minecraft.name}</Accordion.Header>
-          <Accordion.Body as="p" className="accordionBody">
-            Minecraft ID Name: {minecraft.minecraft_id_name}
-          </Accordion.Body>
-          <Accordion.Body as="p" className="accordionBody">
-            Category: {minecraft.category}
-          </Accordion.Body>
-          <Accordion.Body as="p" className="accordionBody">
-            Is Stackable?: {minecraft.stackable}
-          </Accordion.Body>
-          <Accordion.Body as="p" className="accordionBody">
-            Max Stack Size: {minecraft.max_stack_size}
-          </Accordion.Body>
-          {minecraft.durability ? (
-            <AccordionBody as="p" className="accordionBody">
-              Durability: {minecraft.durability}
-            </AccordionBody>
-          ) : null}
-          {minecraft.attack_damage ? (
-            <AccordionBody as="p" className="accordionBody">
-              Attack Damage: {minecraft.attack_damage}
-            </AccordionBody>
-          ) : null}
-          {minecraft.attack_speed ? (
-            <AccordionBody as="p" className="accordionBody">
-              Attack Speed: {minecraft.attack_speed}
-            </AccordionBody>
-          ) : null}
-          {minecraft.armor_points ? (
-            <AccordionBody as="p" className="accordionBody">
-              Armor: {minecraft.armor_points}
-            </AccordionBody>
-          ) : null}
-          {minecraft.armor_toughness ? (
-            <AccordionBody as="p" className="accordionBody">
-              Armor Thoughness: {minecraft.armor_toughness}
-            </AccordionBody>
-          ) : null}
-          {minecraft.knockback_resistance ? (
-            <AccordionBody as="p" className="accordionBody">
-              Knockback Resistance: {minecraft.knockback_resistance}
-            </AccordionBody>
-          ) : null}
-          {minecraft.hunger_points ? (
-            <AccordionBody as="p" className="accordionBody">
-              Hunger Points: {minecraft.hunger_points}
-            </AccordionBody>
-          ) : null}
-          {minecraft.quality ? (
-            <AccordionBody as="p" className="accordionBody">
-              Quality: {minecraft.quality}
-            </AccordionBody>
-          ) : null}
-        </Accordion.Item>
-      </Accordion> */}
     </>
   );
 }
