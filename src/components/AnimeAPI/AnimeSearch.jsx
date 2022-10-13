929e6b2d6ade7c76c2e5470320ee25b06478c2e6
SupraMaxis <kkssccd@gmail.com>
2022-10-12 18:05:51 -0600

    created searchbar component

---
+++ src/components/AnimeAPI/AnimeSearch.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { MdOutlineSearch } from "react-icons/md";
+import styles from "../../styles/searchBar.module.css";
+
+//search bar component for the Anime api
+
+export default function SearchBar({ onSearch }) {
+  const [search, setSearch] = useState("");
+
+  const onChangeHandler = e => {
+    setSearch(e.target.value);
+    if (e.target.value.length === 0) {
+      onSearch(null);
+    }
+  };
+
+  const onClickHandler = async e => {
+    onSearch(search);
+  };
+
+  return (
+    <>
+      <div className={styles.search}>
+        <div className={styles.searchInputs}>
+          <input
+            type="text"
+            onChange={onChangeHandler}
+            placeholder="Buscar algo"
+          />
+          <div className={styles.searchIcon}>
+            <MdOutlineSearch
+              onClick={onClickHandler}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
