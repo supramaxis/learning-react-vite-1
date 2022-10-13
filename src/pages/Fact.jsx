22a18f6d7268b7f6d04f2c68688d3e2f3fdc709a
SupraMaxis <kkssccd@gmail.com>
2022-10-12 21:32:59 -0600

    created a button to fetch more facts

--- src/pages/Fact.jsx
+++ src/pages/Fact.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Button } from "flowbite-react";
 
 function Fact() {
   const [fact, setFact] = useState([]);
@@ -6,16 +7,17 @@ function Fact() {
   const [funFact, setFunFact] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  async function fetchFact() {
+    const data = await fetch(
+      "https://uselessfacts.jsph.pl/random.json?language=en"
+    );
+    const res = await data.json();
+    setFact(res);
+    setLoading(false);
+    // console.log(res);
+  }
+
   useEffect(() => {
-    async function fetchFact() {
-      const data = await fetch(
-        "https://uselessfacts.jsph.pl/random.json?language=en"
-      );
-      const res = await data.json();
-      setFact(res);
-      setLoading(false);
-      // console.log(res);
-    }
     fetchFact();
   }, []);
 
@@ -33,6 +35,11 @@ function Fact() {
           <div className="main-container">
             <p>"{fact.text}"</p>
           </div>
+          <Button
+            className="flex items-center justify-center"
+            onClick={() => fetchFact()}>
+            See New Fact
+          </Button>
           <h1 className="main-container">Random Fun Fact</h1>
           <hr className="my-4 mx-auto w-48 h-1 bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700" />
           <div className="main-container">
