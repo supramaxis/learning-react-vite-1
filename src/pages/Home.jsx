9afdfe85bc13f21576c14b2513d5a8d03238d279
SupraMaxis <kkssccd@gmail.com>
2022-10-12 18:07:32 -0600

    tested react-helmet for body tag styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import { Helmet } from "react-helmet";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Helmet>
+        <style>
+          {` body {
+  background-image: url("../../public/bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+}`}
+        </style>
+      </Helmet>
+      <div className="home">
+        <h1>Home</h1>
+      </div>
+    </>
+  );
 }
 
 export default Home;
