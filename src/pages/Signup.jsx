6ebd8a3eea3cd3b37c864a8f7148e099a3a50e78
supramaxis <kkssccd@gmail.com>
2023-03-08 00:34:48 -0600

    stryled the signup component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,12 @@
-import { Container } from "react-bootstrap";
-import SignUp from "../components/auth/SignUp";
+import { Container } from 'react-bootstrap';
+import SignUp from '../components/auth/SignUp';
 
 export default function Signup() {
   return (
     <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "70vh" }}>
-      <div className="w-100" style={{ maxWidth: "400px" }}>
+      className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '70vh' }}>
+      <div className='w-100' style={{ maxWidth: '400px' }}>
         <SignUp />
       </div>
     </Container>
