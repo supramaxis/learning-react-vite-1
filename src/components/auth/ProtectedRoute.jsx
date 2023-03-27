40f3f564d8ceba35527485001fdaed51b804b1d9
supramaxis <kkssccd@gmail.com>
2023-03-27 17:48:28 -0600

    unfinished login component using supabase auth

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { UserAuth } from "../../context/AuthContext";
+import { Children, useState, useEffect } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import {useItems} from '../../context/ItemsContext';
+import { supabase } from "../../supabase/client";
 
 export default function ProtectedRoute({ children }) {
-  const { user } = UserAuth();
-  if (!user) {
-    return <Navigate to="/login" />;
-  }
-  return children ? children : <Outlet />;
+  // const [user, setUser] = useState(null);
+  const { user } = useItems();
+  const location = useLocation()
+
+  return user ? (
+    <Outlet />
+  ) : (
+    <Navigate to={"/login"} replace state={{path: location.pathname}}/>
+  )
+  // useEffect(() => {
+  //   const usr = supabase.auth.getUser();
+  //   console.log(usr)
+  //   setUser(usr);
+  //   // console.log(usr);
+  // }, []);
+  // console.log(user);
+  // if (!user) {
+  //   return <Navigate to="/login" />;
+  // }
+  // return children
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import LoginC from "../components/auth/LoginC";
+import { useEffect, useState } from 'react';
+import { useItems } from '../context/ItemsContext';
+import { supabase } from '../supabase/client';
+import { useNavigate } from 'react-router-dom';
+import { Button, Label, TextInput } from 'flowbite-react';
+import { HiMail } from 'react-icons/hi';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const { loginWithMagicLink } = useItems();
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    loginWithMagicLink(email);
+  };
+
+  // useEffect(() => {
+  //   const user = supabase.auth.getUser()
+  //   if (user) navigate('/');
+  //   console.log('user has been redirected ');
+  // }, [navigate]);
+
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "70vh" }}>
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <LoginC />
-      </div>
-    </Container>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='mb-2 block'>
+          <Label htmlFor='email4' value='Your email' />
+        </div>
+        <TextInput
+          id='email4'
+          type='email'
+          icon={HiMail}
+          onChange={e => setEmail(e.target.value)}
+          placeholder='name@example.com'
+          required={true}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </div>
   );
 }
